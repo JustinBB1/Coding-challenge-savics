eee4621518e0273dee8a20b82f27cb6af02d0c98
JustinBB1 <79461798+JustinBB1@users.noreply.github.com>
2021-02-22 17:17:05 +0200

    Update addRecord.php

--- addRecord.php
+++ addRecord.php
@@ -1,4 +1,11 @@
 <Doctype HTML>
+<?php
+//take html fields
+foreach ($_POST as $key->$value){
+$a=$key;
+$$a=$value;
+}
+?>
 <HTML lang="en" enctype="multitype_form_data">
 <head>
 <title>coding challenge</title>
@@ -25,3 +32,11 @@
 
 </form>
 </html>
+<?php
+if(isset ($_POST['save'])){
+$req=$con->query("insert into patient () values('$fn','$ln','$gd','$age','$country','$ct')");
+$insert=$con->exec($req);
+
+}
+
+ ?>
