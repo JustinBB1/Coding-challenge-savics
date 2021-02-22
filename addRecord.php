6af88bbd942a014ef36bb5ae956213f57fcda685
JustinBB1 <79461798+JustinBB1@users.noreply.github.com>
2021-02-22 17:03:21 +0200

    Create addRecord.php

---
+++ addRecord.php
@@ -0,0 +1,27 @@
+<Doctype HTML>
+<HTML lang="en" enctype="multitype_form_data">
+<head>
+<title>coding challenge</title>
+<form method="post" action="">
+<h2>New medical record</h2>
+<input type="text" name="fn" placeholder="First name:">
+<input type="text" name="ln" placeholder="last name:">
+
+<input type="checkbox" name="gd" label="Male">
+<input type="checkbox" name="gd" label="Female">
+<input type="number" name="age" placeholder="Age:">
+<input type="text" name="fn" placeholder="First name">
+<select name="country">
+<option value=DRC">DR. Congo</option>
+<option value=name="..."> Other</option>
+
+</select>
+<input type="submit" name="save" value="save">
+<table>
+<th> List of records
+<td><tr></tr></td>
+</th>
+</table>
+
+</form>
+</html>
