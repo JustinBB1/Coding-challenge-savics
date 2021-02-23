3a96979cd61166e7185c93c5149247f142be2c66
JustinBB1 <79461798+JustinBB1@users.noreply.github.com>
2021-02-23 06:20:33 +0200

    Create checkvalues.js
    A JavaScript file for input controler

---
+++ checkvalues.js
@@ -0,0 +1,11 @@
+<script>
+public boolean checkvalues (){
+var fn=myform['fn'];
+var sn=myform['ln'];
+var ct=myform['ct'];
+if(fn=="" || sn="" ||ct==""){
+return null;
+alert('Some fields are empty');
+}
+}
+</script>
